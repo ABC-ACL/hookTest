52b6c8d646d4b3caaecbac71b34e9b8036d39af0
ABC-ACL <abc@aclsi.pt>
2024-04-15 22:01:15 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DynamicHooksModule } from 'ngx-dynamic-hooks';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,
+    DynamicHooksModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { DynamicHooksModule, HookParserEntry } from 'ngx-dynamic-hooks';
+// import { TestComponent } from './test/test.component';
+
+const componentParsers: Array<HookParserEntry> = [
+  // {component: TestComponent},
+  { component: {
+      importPromise: () => import('./test/test.component'),
+      importName: "TestComponent"
+    },
+    selector: "app-test"
+  },
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(DynamicHooksModule.forRoot({
+      globalParsers: componentParsers,
+      globalOptions: { sanitize: false }
+    })),]
 };
